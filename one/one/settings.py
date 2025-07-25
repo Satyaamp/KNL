1837a7240ea44751fd405937be86a118823adda4
SATYAM KUMAR <cbse821@gmail.com>
2025-07-25 21:34:32 +0530

    Deployment

--- one/one/settings.py
+++ one/one/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^4xws33bpy51$cdlw6+rxgap$qnh1nrod1@ix@$v_uw#&i5gsr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
